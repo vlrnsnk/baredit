06519313d054b1a04837a848d12b02b055abf532
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-12-03 20:14:42 -0500

    Fix responsive layout of header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,8 +8,10 @@ const Header = ({ handleBurgerClick }) => {
     <header className="flex flex-col gap-4 bg-gray-200 p-4">
       <nav className="container mx-auto flex flex-row justify-between items-center gap-x-4">
         <Burger handleBurgerClick={handleBurgerClick} />
-        <Logo />
-        <h1 className="md:me-auto text-3xl font-bold text-orange-500">BareDit</h1>
+        <div className="flex gap-2 sm:gap-4 md:mx-auto">
+          <Logo />
+          <h1 className="text-3xl font-bold text-orange-500">BareDit</h1>
+        </div>
         <ThemeSwitcher />
       </nav>
       <hr className="bg-black size-px w-auto" />
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,8 +2,15 @@ import logo from 'assets/logo.png';
 
 const Logo = () => {
   return (
-    <a className="shrink-0" href="https://baredit.netlify.app/">
-      <img className="size-9 rounded-full" src={logo} alt="BareDit Logo" />
+    <a
+      className="shrink-0"
+      href="https://baredit.netlify.app/"
+    >
+      <img
+        className="size-9 rounded-full"
+        src={logo}
+        alt="BareDit Logo"
+      />
     </a>
   );
 };
