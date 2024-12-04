23a929f7b6b25b84494ce898f53e459fbf463a23
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-12-03 21:33:07 -0500

    Done layout for Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,24 +2,21 @@ import { Burger } from "components/Burger/Burger";
 import { Logo } from "components/Logo/Logo";
 import { SearchBar } from "components/SearchBar/SearchBar";
 import { ThemeSwitcher } from "components/ThemeSwitcher/ThemeSwitcher";
-import { SearchIcon } from "components/SearchIcon/SearchIcon";
 
 const Header = ({ handleBurgerClick }) => {
   return (
     <header className="flex flex-col gap-4 bg-gray-200 p-4">
       <nav className="container mx-auto flex flex-row justify-between items-center sm:gap-x-4 sm:px-4 md:px-8">
         <Burger handleBurgerClick={handleBurgerClick} />
-        <div className="flex gap-2 sm:gap-4 mx-auto">
+        <div className="flex gap-2 sm:gap-4 sm:mx-auto hidden logo me-auto">
           <Logo />
-          <h1 className="text-3xl font-bold text-orange-500">BareDit</h1>
+          <h1 className="text-3xl font-bold text-orange-500 hidden heading">BareDit</h1>
         </div>
         <div className="flex sm:gap-2">
-          <SearchIcon />
+          <SearchBar />
           <ThemeSwitcher />
         </div>
       </nav>
-      {/* <hr className="bg-black size-px w-auto" /> */}
-      {/* <SearchBar /> */}
     </header>
   );
 };
