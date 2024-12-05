ec88f202521a491d324974fd24f21bc39aa5bbad
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-12-05 18:50:26 -0500

    Add hover animation to ScrollToTop button

--- src/components/ScrollToTopButton/ScrollToTopButton.jsx
+++ src/components/ScrollToTopButton/ScrollToTopButton.jsx
@@ -25,7 +25,7 @@ const ScrollToTopButton = () => {
   return (
     <button
       onClick={scrollToTop}
-      className={`size-12 rounded-full text-orange-400 fixed bottom-5 right-5 sm:bottom-10 sm:right-10 p-2 bg-black bg-opacity-60 shadow-lg transition-opacity duration-500 ease-in-out ${isVisible ? `opacity-100` : `opacity-0`}`}
+      className={`size-12 rounded-full text-orange-400 fixed bottom-5 right-5 sm:bottom-10 sm:right-10 p-2 bg-black/80 dark:bg-white/80 hover:bg-white/80 dark:hover:bg-black/80 shadow-lg transition-opacity animation-color duration-500 ease-in-out ${isVisible ? `opacity-100` : `opacity-0`}`}
     >
       <ArrowUp />
     </button>
