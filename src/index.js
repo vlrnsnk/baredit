3e861b063077c7240e504ddd01c4a930fdf16a99
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-12-08 20:42:38 -0500

    Add PWA functionality

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './app/App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,3 +12,5 @@ root.render(
     <App />
   </Provider>
 );
+
+serviceWorkerRegistration.register();
